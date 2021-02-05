175f0e72d201edd561c8ec7441063071c41b28ea
Кирилл Шубарин <kirill.shubarin@gmail.com>
2021-02-05 12:33:30 +0300

    2 задача. Тренировки в полёте

--- Готовимся к миссии.py
+++ Готовимся к миссии.py
@@ -11,6 +11,19 @@ def index(title='Главная'):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    if 'инженер' in prof or 'строитель' in prof:
+        text = 'Инженерный тренажер'
+    else:
+        text = 'Научный тренажер'
+    context = {
+        'title': 'Тренировка',
+        'text': text
+    }
+    return render_template('training.html', **context)
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(port=8080, host='localhost')
