6a0ddd14275ed8a6f36ab2bf61a6c75eb3fe1042
Кирилл Шубарин <kirill.shubarin@gmail.com>
2021-02-08 16:08:13 +0300

    5 задача. По каютам!

--- Готовимся к миссии.py
+++ Готовимся к миссии.py
@@ -73,6 +73,16 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/distribution')
+def distribution(team=('Ридли Скотт', 'Энди Уир',
+                       'Марк Уотни', 'Венката Капур',
+                       'Тедди Сандерс', 'Шон Бин')):
+    context = {
+        'title': 'Размещение по каютам',
+        'team': team,
+    }
+    return render_template('distribution.html', **context)
+
 if __name__ == '__main__':
     app.debug = True
     app.run(port=8080, host='localhost')
